8aa1377511d03a1fb037dbd44f3264aa29018d32
shivank yadav <shxvvank@gmail.com>
2022-05-23 22:20:05 +0530

    Update v1

---
+++ BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController:Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public PieController(IPieRepository pieRepository,ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel();
+            pieListViewModel.Pies = _pieRepository.AllPies;
+            pieListViewModel.CurrentCategory = "Cheese Cakes";
+            return View(pieListViewModel);
+        }
+    }
+}
--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -13,8 +13,8 @@ namespace BethanysPieShop.Models
         public string LongDescription { get; set; }
         public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
-        public string imageUrl { get; set; }
-        public string ImageThyumbnailUrl { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
